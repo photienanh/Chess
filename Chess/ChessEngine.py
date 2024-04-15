922d7df7b0ebdb58a345e9b8e5329ab043333c36
Phó Tiến Anh <pvtatienanh@gmail.com>
2024-04-15 22:27:19 +0700

    Add methods

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -1,27 +1,80 @@
-def __init__(self):
-    self.board = [
-        ["bR","bN","bB","bQ","bK","bB","bN","bR"],
-        ["bp","bp","bp","bp","bp","bp","bp","bp"],
-        ["--","--","--","--","--","--","--","--"],
-        ["--","--","--","--","--","--","--","--"],
-        ["--","--","--","--","--","--","--","--"],
-        ["--","--","--","--","--","--","--","--"],
-        ["wp","wp","wp","wp","wp","wp","wp","wp"],
-        ["wR","wN","wB","wQ","wK","wB","wN","wR"]]
-    self.moveFunctions = {'p': self.getPawnMoves, 'R': self.getRookMoves, 'N':self.getKnightMoves,
-                          'B': self.getBishopMoves, 'Q': self.getQueenMoves, 'K': self.getKingMoves}
-    self.whiteToMove = True
-    self.moveLog = []
-    self.whiteKingLocation = (7,4)
-    self.blackKingLocation = (0,4)
-def makeMove(self, move):
-    self.board[move.startRow][move.startCol] = "--"
-    self.board[move.endRow][move.endCol] = move.pieceMoved
-    self.moveLog.append(move)
-    self.whiteToMove = not self.whiteToMove
-def undoMove(self):
-    if len(self.moveLog) != 0:
-        move = self.moveLog.pop()
-        self.board[move.startRow][move.startCol] = move.pieceMoved
-        self.board[move.endRow][move.endCol] = move.pieceCaptured
+class GameState():
+    def __init__(self):
+        self.board = [
+            ["bR","bN","bB","bQ","bK","bB","bN","bR"],
+            ["bp","bp","bp","bp","bp","bp","bp","bp"],
+            ["--","--","--","--","--","--","--","--"],
+            ["--","--","--","--","--","--","--","--"],
+            ["--","--","--","--","--","--","--","--"],
+            ["--","--","--","--","--","--","--","--"],
+            ["wp","wp","wp","wp","wp","wp","wp","wp"],
+            ["wR","wN","wB","wQ","wK","wB","wN","wR"]]
+        self.moveFunctions = {'p': self.getPawnMoves, 'R': self.getRookMoves, 'N':self.getKnightMoves,
+                            'B': self.getBishopMoves, 'Q': self.getQueenMoves, 'K': self.getKingMoves}
+        self.whiteToMove = True
+        self.moveLog = []
+        self.whiteKingLocation = (7,4)
+        self.blackKingLocation = (0,4)
+    def makeMove(self, move):
+        self.board[move.startRow][move.startCol] = "--"
+        self.board[move.endRow][move.endCol] = move.pieceMoved
+        self.moveLog.append(move)
         self.whiteToMove = not self.whiteToMove
+        if move.pieceMoved == "wK":
+            self.whiteKingLocation = (move.endRow, move.endCol)
+        elif move.pieceMoved == "bK":
+            self.blackKingLocation = (move.endRow, move.endCol)
+    def undoMove(self):
+        if len(self.moveLog) != 0:
+            move = self.moveLog.pop()
+            self.board[move.startRow][move.startCol] = move.pieceMoved
+            self.board[move.endRow][move.endCol] = move.pieceCaptured
+            self.whiteToMove = not self.whiteToMove
+            if move.pieceMoved == "wK":
+                self.whiteKingLocation = (move.startRow, move.startCol)
+            elif move.pieceMoved == "bK":
+                self.blackKingLocation = (move.startRow, move.startCol)
+    def getValidMoves(self):
+        moves = self.getAllPossibleMoves()
+        nums = [0,1,2,3,3,4,5]
+        for i in range(len(moves)-1,-1,-1):
+            self.makeMove(moves[i])
+            oppMoves = self.getAllPossibleMoves()
+        return self.getAllPossibleMoves()
+    def inCheck(self):
+        if self.whiteToMove:
+            return self.squareUnderAttack(self.whiteKingLocation[0],self.whiteKingLocation[1])
+        else:
+            return self.squareUnderAttack(self.blackKingLocation[0],self.blackKingLocation[1])
+    def getAllPossibleMoves(self):
+        pass
+    def getPawnMoves(self, r, c, moves):
+        pass
+    def getRookMoves(self, r, c, moves):
+        pass
+class Move():
+    ranksToRows = {"1":7,"2":6,"3":5,"4":4,
+                   "5":3,"6":2,"7":1,"8":0}
+    rowToRanks = {v: k for k, v in ranksToRows.items()}
+    fileToCols = {"a":0,"b":1,"c":2,"d":3,
+                  "e":4,"f":5,"g":6,"h":7}
+    colsToFiles = {v: k for k, v in fileToCols.items()}
+    def __init__(self, startSq, endSq, board):
+        self.startRow = startSq[0]
+        self.startCol = startSq[1]
+        self.endRow = endSq[0]
+        self.endCol = endSq[1]
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
+        self.moveID = self.startRow * 1000 + self.startCol * 100 + self.endRow * 10 + self.endCol
+        print(self.moveID)
+    def __eq__(self, other):
+        if isinstance(other, Move):
+            return self.moveID == other.moveID
+        return False
+    def squareUnderAttack(self, r, c):
+        pass
+    def getChessLocation(self):
+        pass
+    def getRankFile(self, r, c):
+        pass
