11eab10382e016554bf8d8714806d138c6658daf
NguyenPhuongDng <144362042+NguyenPhuongDng@users.noreply.github.com>
2024-05-02 20:58:17 +0700

    hmm

---
+++ Evalute.py
@@ -0,0 +1,124 @@
+piece_value = {
+    {"K": 0, "Q": 10, "R": 5, "B": 3, "N": 3, "p": 1}
+}
+
+pawnEvalWhite = [
+    0,  0,  0,  0,  0,  0,  0,  0,
+    5, 10, 10, -20, -20, 10, 10,  5,
+    5, -5, -10,  0,  0, -10, -5,  5,
+    0,  0,  0, 20, 20,  0,  0,  0,
+    5,  5, 10, 25, 25, 10,  5,  5,
+    10, 10, 20, 30, 30, 20, 10, 10,
+    50, 50, 50, 50, 50, 50, 50, 50,
+    0, 0, 0, 0, 0, 0, 0, 0
+]
+pawnEvalBlack = list(reversed(pawnEvalWhite))
+
+knightEval = [
+    -50, -40, -30, -30, -30, -30, -40, -50,
+    -40, -20, 0, 0, 0, 0, -20, -40,
+    -30, 0, 10, 15, 15, 10, 0, -30,
+    -30, 5, 15, 20, 20, 15, 5, -30,
+    -30, 0, 15, 20, 20, 15, 0, -30,
+    -30, 5, 10, 15, 15, 10, 5, -30,
+    -40, -20, 0, 5, 5, 0, -20, -40,
+    -50, -40, -30, -30, -30, -30, -40, -50
+]
+
+bishopEvalWhite = [
+    -20, -10, -10, -10, -10, -10, -10, -20,
+    -10, 5, 0, 0, 0, 0, 5, -10,
+    -10, 10, 10, 10, 10, 10, 10, -10,
+    -10, 0, 10, 10, 10, 10, 0, -10,
+    -10, 5, 5, 10, 10, 5, 5, -10,
+    -10, 0, 5, 10, 10, 5, 0, -10,
+    -10, 0, 0, 0, 0, 0, 0, -10,
+    -20, -10, -10, -10, -10, -10, -10, -20
+]
+bishopEvalBlack = list(reversed(bishopEvalWhite))
+
+rookEvalWhite = [
+    0, 0, 0, 5, 5, 0, 0, 0,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    -5, 0, 0, 0, 0, 0, 0, -5,
+    5, 10, 10, 10, 10, 10, 10, 5,
+    0, 0, 0, 0, 0, 0, 0, 0
+]
+rookEvalBlack = list(reversed(rookEvalWhite))
+
+queenEval = [
+    -20, -10, -10, -5, -5, -10, -10, -20,
+    -10, 0, 0, 0, 0, 0, 0, -10,
+    -10, 0, 5, 5, 5, 5, 0, -10,
+    -5, 0, 5, 5, 5, 5, 0, -5,
+    0, 0, 5, 5, 5, 5, 0, -5,
+    -10, 5, 5, 5, 5, 5, 0, -10,
+    -10, 0, 5, 0, 0, 0, 0, -10,
+    -20, -10, -10, -5, -5, -10, -10, -20
+]
+
+kingEvalWhite = [
+    20, 30, 10, 0, 0, 10, 30, 20,
+    20, 20, 0, 0, 0, 0, 20, 20,
+    -10, -20, -20, -20, -20, -20, -20, -10,
+    20, -30, -30, -40, -40, -30, -30, -20,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30,
+    -30, -40, -40, -50, -50, -40, -40, -30
+]
+kingEvalBlack = list(reversed(kingEvalWhite))
+
+kingEvalEndGameWhite = [
+    50, -30, -30, -30, -30, -30, -30, -50,
+    -30, -30,  0,  0,  0,  0, -30, -30,
+    -30, -10, 20, 30, 30, 20, -10, -30,
+    -30, -10, 30, 40, 40, 30, -10, -30,
+    -30, -10, 30, 40, 40, 30, -10, -30,
+    -30, -10, 20, 30, 30, 20, -10, -30,
+    -30, -20, -10,  0,  0, -10, -20, -30,
+    -50, -40, -30, -20, -20, -30, -40, -50
+]
+kingEvalEndGameBlack = list(reversed(kingEvalEndGameWhite))
+
+def evaluate_piece(gs, piece, square, end_game) -> int:
+    piece_type = piece.piece_type
+    mapping = []
+    if piece_type == "p":
+        mapping = pawnEvalWhite if gs.whiteToMove else pawnEvalBlack
+    if piece_type == "K":
+        mapping = knightEval
+    if piece_type == "B":
+        mapping = bishopEvalWhite if gs.whiteToMove else bishopEvalBlack
+    if piece_type == "R":
+        mapping = rookEvalWhite if gs.whiteToMove else rookEvalBlack
+    if piece_type == "Q":
+        mapping = queenEval
+    if piece_type == "K":
+        if end_game:
+            mapping = (
+                kingEvalEndGameWhite
+                if gs.whiteToMove
+                else kingEvalEndGameBlack
+            )
+        else:
+            mapping = kingEvalWhite if gs.whiteToMove else kingEvalBlack
+
+    return mapping[square]
+
+def evaluate_board(board) -> float:
+    total = 0
+    end_game
+
+    for row in board:
+        for square in row:
+            piece = board.piece_at(square)
+            if not piece:
+                continue
+
+            value = piece_value[piece.piece_type] + evaluate_piece(piece, square, end_game)
+            total += value if square[0] == "b" else -value
+    return total
