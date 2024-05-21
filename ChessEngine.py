d616cf354184978e5d8ac745f9ca8b30fa77e903
sayohyeah <sayohyeahqpqp@gmail.com>
2024-05-21 20:30:22 +0700

    delete

--- ChessEngine.py
+++ ChessEngine.py
@@ -567,14 +567,7 @@ class GameState():
          self.board[r][c - 4] == allyColor + 'R':
             moves.append(Move((r, c), (r, c - 2), self.board, castle = True))
 
-class Move():
-    # ranksToRows = {"1":7,"2":6,"3":5,"4":4,
-    #                "5":3,"6":2,"7":1,"8":0}
-    # rowsToRanks = {v: k for k, v in ranksToRows.items()}
-    # filesToCols = {"a":0,"b":1,"c":2,"d":3,
-    #               "e":4,"f":5,"g":6,"h":7}
-    # colsToFiles = {v: k for k, v in filesToCols.items()}
-    
+class Move():    
     def __init__(self, startSq, endSq, board, enPassant = False, castle = False, promotedPiece = None):
         self.startRow = startSq[0]
         self.startCol = startSq[1]
